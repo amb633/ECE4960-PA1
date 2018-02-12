dda360e555a13830c642bf3c7f3ed7a9cf979787
hm535 <hm535@users.noreply.github.com>
2018-02-11 21:21:13 -0500

    testing compilation

---
+++ test/functions.h
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int factorial ( int x );
+double factorial ( double x );
+
+int fibonacci ( int sequence );
+double fibonacci_d ( int sequence );
---
+++ test/program1.cpp
@@ -0,0 +1,26 @@
+#include "functions.h"
+
+int main() {
+	cout << endl;
+
+	int int_factorial = factorial ( 20 );
+	cout << endl;
+
+	/*int int_fibonacci = fibonacci( 50 );
+	cout << endl;
+
+	int int_zero;
+	int zero = 0;
+
+	/*try {
+		int_zero = 2/zero;
+		throw -1;
+	}
+
+	catch ( int err ) {
+		cout << " error in divsion " << endl << endl;
+	}*/
+
+	return 0;
+}
+
